629c8b1f8895612d9acecf24748566c99f90100f
Maxim Glukhov <glukhov_maxim@mail.ru>
2023-01-16 11:41:22 +0700

    Initial commit

---
+++ src/test/java/page/VerificationPage.java
@@ -0,0 +1,34 @@
+package page;
+
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.support.FindBy;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.page;
+
+public class VerificationPage {
+    @FindBy(css = "[data-test-id='code'] input")
+    private SelenideElement codeField;
+    @FindBy(css="[data-test-id='action-verify']")
+    private SelenideElement verifyButton;
+    @FindBy(css="[data-test-id='error-notification']")
+    private SelenideElement errorNotification;
+
+    public void verifyVerificationPageVisibility(){
+        codeField.shouldBe(visible);
+    }
+
+    public void errorNotificationVisibility(){
+        errorNotification.shouldBe(visible);
+    }
+
+    public ApplicationFormPage validVerify(String verificationCode) {
+        verify(verificationCode);
+        return page(ApplicationFormPage.class);
+    }
+
+    public void verify(String verificationCode) {
+        codeField.setValue(verificationCode);
+        verifyButton.click();
+    }
+}
